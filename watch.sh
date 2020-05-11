9e6aac754e8b83fc07a2c280aa6c0ceae8ce0f8e
ender <xuender@gmail.com>
2020-05-11 10:04:58 +0800

    style

--- watch.sh
+++ watch.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# 监听网页变化
-user=`whoami`
-command="/home/$user/bin/webhash"
+command="/home/$(whoami)/bin/webhash"
 report=`$command -r`
 
 if [ "$report" != "" ]; then
-  XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send "webhash提示" "$report" -u critical -i applications-internet
+  XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send "Webhash 提示:" "$report" -u critical -i applications-internet
   $command update
 fi
