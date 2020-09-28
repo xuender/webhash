9e342d799a2a5b02bc0bee8bb028fefc725869d6
ender <xuender@gmail.com>
2020-09-28 09:54:12 +0800

    build(mod): 依赖版本升级

--- go.mod
+++ go.mod
@@ -1,20 +1,22 @@
 module github.com/xuender/webhash
 
-go 1.14
+go 1.15
 
 require (
-	github.com/asaskevich/govalidator v0.0.0-20200428143746-21a406dcc535
+	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/minio/highwayhash v1.0.0
+	github.com/magiconair/properties v1.8.4 // indirect
+	github.com/minio/highwayhash v1.0.1
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/mitchellh/mapstructure v1.3.0
-	github.com/pelletier/go-toml v1.7.0 // indirect
-	github.com/spf13/afero v1.2.2 // indirect
+	github.com/mitchellh/mapstructure v1.3.3
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/spf13/afero v1.4.0 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.7.0
-	golang.org/x/sys v0.0.0-20200509044756-6aff5f38e54f // indirect
-	gopkg.in/ini.v1 v1.56.0 // indirect
+	github.com/spf13/viper v1.7.1
+	golang.org/x/sys v0.0.0-20200926100807-9d91bd62050c // indirect
+	gopkg.in/ini.v1 v1.61.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- install.sh
+++ install.sh
@@ -1,52 +1,55 @@
 #!/bin/bash
 BINARY=webhash
 BIN_PATH=$HOME/bin
+
 UNAME=$(uname)
 ARCH=$(uname -m)
-if [ "$UNAME" = "Darwin" ] ; then
-  if [ "$ARCH" = "x86_64" ] ; then
-    PLATFORM="darwin_amd64"
-  fi
-elif [ "$UNAME" = "Linux" ] ; then
-  if [ "$ARCH" = "i686" ] ; then
-    PLATFORM="linux_386"
-  elif [ "$ARCH" = "x86_64" ] ; then
-    PLATFORM="linux_amd64"
-  fi
+if [ "$UNAME" = "Darwin" ]; then
+	if [ "$ARCH" = "x86_64" ]; then
+		PLATFORM="darwin_amd64"
+	fi
+elif [ "$UNAME" = "Linux" ]; then
+	if [ "$ARCH" = "i686" ]; then
+		PLATFORM="linux_386"
+	elif [ "$ARCH" = "x86_64" ]; then
+		PLATFORM="linux_amd64"
+	fi
 fi
+
 DEST=$BIN_PATH/$BINARY
 # latest
 get_latest_release() {
-  curl --silent "https://api.github.com/repos/xuender/$BINARY/releases/latest" | # Get latest release from GitHub api
-    grep '"tag_name":' |                                            # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+	curl --silent "https://api.github.com/repos/xuender/$BINARY/releases/latest" | # Get latest release from GitHub api
+		grep '"tag_name":' |                                                          # Get tag line
+		sed -E 's/.*"([^"]+)".*/\1/'                                                  # Pluck JSON value
 }
 # 安装
-install () {
-  echo "Installing $BINARY..."
-  if [ "$UNAME" != "Linux" ] && [ "$UNAME" != "Darwin" ] && [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "i686" ]; then
-    echo "Sorry, OS/Architecture not supported: ${UNAME}/${ARCH}. Download binary from https://github.com/xuender/${BINARY}/releases"
-    exit 1
-  fi
-  mkdir -p $BIN_PATH
-  LATEST=$(get_latest_release)
-  echo "$LATEST"
-  URL="https://github.com/xuender/webhash/releases/download/$LATEST/${BINARY}_${PLATFORM}"
-  echo "download $URL"
-  rm -rf $DEST
-  curl -L $URL -o $DEST
-  chmod +x $DEST
+install() {
+	echo "Installing $BINARY..."
+	if [ "$UNAME" != "Linux" ] && [ "$UNAME" != "Darwin" ] && [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "i686" ]; then
+		echo "Sorry, OS/Architecture not supported: ${UNAME}/${ARCH}. Download binary from https://github.com/xuender/${BINARY}/releases"
+		exit 1
+	fi
+	mkdir -p $BIN_PATH
+	LATEST=$(get_latest_release)
+	echo "$LATEST"
+	URL="https://github.com/xuender/webhash/releases/download/$LATEST/${BINARY}_${PLATFORM}"
+	echo "download $URL"
+	rm -rf $DEST
+	curl -L $URL -o $DEST
+	chmod +x $DEST
 }
 
-run () {
-  echo "Running webhash..."
-  webhash
+run() {
+	echo "Running webhash..."
+	webhash
 }
 
 if [ -f $DEST ]; then
-   echo "Webhash is already installed."
+	echo "Webhash is already installed."
 else
-   echo "Webhash is not installed."
+	echo "Webhash is not installed."
 fi
+
 install
 run
